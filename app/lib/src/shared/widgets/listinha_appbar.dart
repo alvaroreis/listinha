00ae18d75647db7b45a013620ee68e369ffc5e56
Álvaro Barros <alvaroluiz.rb@gmail.com>
2023-02-10 14:36:10 -0300

    feat: add themes

---
+++ app/lib/src/shared/widgets/listinha_appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListinhaAppbar extends StatelessWidget {
+  const ListinhaAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
