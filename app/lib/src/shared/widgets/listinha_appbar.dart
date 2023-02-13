92010f701a97d50aa8948102a0fd22ab7114cb44
Álvaro Barros <alvaroluiz.rb@gmail.com>
2023-02-13 14:13:49 -0300

    feat: add listinha appbar

--- app/lib/src/shared/widgets/listinha_appbar.dart
+++ app/lib/src/shared/widgets/listinha_appbar.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
-class ListinhaAppbar extends StatelessWidget {
-  const ListinhaAppbar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Container(),
-    );
-  }
+class ListinhaAppbar extends AppBar {
+  ListinhaAppbar({
+    super.key,
+    super.actions,
+    Widget title = const Text('LISTINHA'),
+    super.systemOverlayStyle,
+    super.leading,
+  }) : super(title: title);
 }
